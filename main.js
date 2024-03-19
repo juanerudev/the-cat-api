a5593b4793fff1ac49f0474d36b2fdf35084969a
Juan Rodriguez <juanestebanrodriguezurrea@gmail.com>
2024-03-18 20:14:15 -0500

    Se usa sintaaxis async await y se actualizan algunas cosas a la hora de obtener la informacion de la api

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 const URL = 'https://api.thecatapi.com/v1/images/search';
 
-cargarGatitos = () => {
-    fetch(URL)
-    .then((response) => response.json())
-    .then((data) => {
+document.addEventListener('DOMContentLoaded', getCat)
+
+async function getCat() {
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
         const img = document.querySelector('img');
         img.src = data[0].url;
-    })
+    } catch (error) {
+        console.error(`Error al conectarse con la api: ${error}`);
+    }
 }
 
-cargarGatitos();
-
 const button = document.querySelector('button');
-
-button.addEventListener('click', () => {
-    cargarGatitos()
-})
+button.onclick = getCat
