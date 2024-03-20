653e890202731f720a412844bc2564b0562af811
Juan Rodriguez <juanestebanrodriguezurrea@gmail.com>
2024-03-19 23:17:34 -0500

    Cambios a la API, se agregó un query parameter para indicar el limite de fotos que quiero, pero no funciona del todo ya que me trae siempre 10 imagenes entonces le doy manejo con un input de tipo number y un ciclo para mostrar la cantidad de imagenes que desee ver el usuario, el input está limitado entre del 1 al 10

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
 
 document.addEventListener('DOMContentLoaded', getCat)
 
 async function getCat() {
     try {
-        const response = await fetch(URL);
+        const response = await fetch(API_URL);
         const data = await response.json();
-        const img = document.querySelector('img');
-        img.src = data[0].url;
+
+        const count = document.querySelector('#count');
+
+        const images = document.querySelector('.cat-image');
+        images.innerHTML = "";
+        console.log(images);
+
+        for (let index = 0; index < count.value; index++) {
+            const element = data[index];
+            
+            console.log(element.url);
+
+            const img = document.createRange().createContextualFragment(
+                `
+                <img src="${element.url}" alt="Foto gatito aleatorio">
+                `
+            )
+
+            images.append(img)
+        }
+
+        console.log(images);
+
+        console.log(data);
     } catch (error) {
         console.error(`Error al conectarse con la api: ${error}`);
     }
